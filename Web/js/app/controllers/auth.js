836c6ada7e9dd33e014581e4feb640df26d95302
WebDevJL <webdev.jl@gmail.com>
2014-05-26 19:02:30 +0200

    added the login method for ajax call

--- Web/js/app/controllers/auth.js
+++ Web/js/app/controllers/auth.js
@@ -1,7 +1,9 @@
 $(document).ready(function() {
-  validator.requiredInput();    
-    $("#btn_login").click(function() {
-    auth.retrieveCredentials();
+  validator.requiredInput();
+  $("#btn_login").click(function() {
+    var post_data = auth.retrieveCredentials();
+    if (post_data["result"] === "success")
+      auth.login(post_data);
   });
 });
 /***********
@@ -11,12 +13,22 @@ $(document).ready(function() {
 (function(auth) {
   auth.retrieveCredentials = function() {
     var email = $("input[name=email]").val();
-    var pwd = $("input[name=password]").val(); 
+    var pwd = $("input[name=password]").val();
     var valid = validator.checkAndClean();
     if (valid) {
-        toastr.success("<p>Logging in! :)</p><p><u>Email:</u> "+email+" ; </p><p><u>Password:</u> ****;</p>");//TODO: use resource manager
+      toastr.success("<p>Logging in! :)</p><p><u>Email:</u> " + email + " ; </p><p><u>Password:</u> ****;</p>");//TODO: use resource manager
+      return {"result": "success", "email": email, "pwd": pwd};
     } else {
-        toastr.error("Try again...");//TODO: use resource manager
+      toastr.error("Try again...");//TODO: use resource manager
     }
+  };
+  auth.login = function(credentials) {
+    var result = $.ajax({
+      url: "auth",
+      data: credentials, //data,
+      type: 'POST',
+      contentType: 'application/json',
+      dataType: 'json'
+    });
   };
 }(window.auth = window.auth || {}));
