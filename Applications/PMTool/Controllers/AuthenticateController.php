4c4986ad5c26c970b9d0cacbfaa8239d7cc758af
WebDevJL <webdev.jl@gmail.com>
2014-05-27 20:16:35 +0200

    PDO: bug fix to remove mysql_real_escape

--- Applications/PMTool/Controllers/AuthenticateController.php
+++ Applications/PMTool/Controllers/AuthenticateController.php
@@ -14,11 +14,12 @@ class AuthenticateController extends \Library\BaseController {
 
     $this->page->addVar('resx', $this->app->i8n->getLocalResourceArray($resourceFileKey));
   }
-  
+
   /*
    * Method that receives the call from JS Client to login a user
    * Return the result whether the user can be logged in
    */
+
   public function executeAuthenticate(\Library\HttpRequest $rq) {
     $resourceFileKey = "login";
     $result = array(
@@ -32,18 +33,13 @@ class AuthenticateController extends \Library\BaseController {
 
     //Let's retrieve the inputs from POST
     //First, check the inputs. If valid, we'll continue.
-    if (\Library\Core\Utilities\FormUtility::CleanseInput($rq->postData("username")) &&
-            \Library\Core\Utilities\FormUtility::CleanseInput($rq->postData("pwd"))) {
-      $result["user_sent"] = array(
-          "username" => "test",//$rq->postData("username"),
-          "pwd" => "password"//$rq->postData("pwd")
-      );
-      $result["user_returned"] = $manager->selectOne($result["user_sent"]);
-      header('Content-Type: application/json');
-      echo json_encode($result["user_returned"], 128);
-    } else {
-      echo json_encode($result["user_returned"], 128);
-    }
+    $result["user_sent"] = array(
+        "username" => "test", //$rq->postData("username"),
+        "pwd" => "password"//$rq->postData("pwd")
+    );
+    $result["user_returned"] = $manager->selectOne($result["user_sent"]);
+    header('Content-Type: application/json');
+    echo json_encode($result["user_returned"], 128);
   }
 
 }
--- Library/DAL/Models/LoginManager_PDO.php
+++ Library/DAL/Models/LoginManager_PDO.php
@@ -6,11 +6,11 @@ class LoginManager_PDO extends \Library\DAL\BaseManager {
 
   public function selectOne($user_obj) {
     $sql = 'SELECT * FROM project_manager where username = \''. $user_obj["username"] . '\' LIMIT 0, 1;';
-    $requete = $this->dao->query($sql);
-    $requete->setFetchMode(\PDO::FETCH_CLASS | \PDO::FETCH_PROPS_LATE, '\Library\BO\ProjectManager');
+    $query = $this->dao->query($sql);
+    $query->setFetchMode(\PDO::FETCH_CLASS | \PDO::FETCH_PROPS_LATE, '\Library\BO\ProjectManager');
     
-    $pm = $requete->fetchAll();
-    $requete->closeCursor();
+    $pm = $query->fetchAll();
+    $query->closeCursor();
     
     return $pm;    
   }
