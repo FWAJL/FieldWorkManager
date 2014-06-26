35df9f1e1e921374c3ae7e04b9db440a2a84c2a9
WebDevJL <webdev.jl@gmail.com>
2014-06-26 06:10:38 +0200

    Build the "Add new project" form

--- Applications/PMTool/Views/Project/index.php
+++ Applications/PMTool/Views/Project/index.php
@@ -5,87 +5,112 @@
   <table border="0" align="center" cellpadding="0" cellspacing="0" class="add-new-p">
     <tr>
       <td align="left" valign="top" style="padding:3px">
-        <p><span>Project Name:</span></p></td>
+        <p><label><?php echo $resx["project_name"]; ?></label></p></td>
       <td width="59%" align="left" valign="top"><input type="text" size="15"></td>
     </tr>
     <tr>
-      <td align="left" valign="top" style="padding:3px"><p><span>Project Description:</span></p></td>
+      <td align="left" valign="top" style="padding:3px">
+        <p><label><?php echo $resx["project_desc"]; ?></label></p>
+      </td>
       <td align="left" valign="top"><input name="" type="text" size="15"></td>
     </tr>
-
+    <tr>
+      <td align="left" valign="top" style="padding:3px">
+        <p><label><?php echo $resx["project_desc"]; ?></label></p>
+      </td>
+      <td align="left" valign="top"><input name="" type="text" size="15"></td>
+    </tr>
+    <tr>
+      <td align="left" valign="top" style="padding:3px">
+        <p><label><?php echo $resx["project_desc"]; ?></label></p>
+      </td>
+      <td align="left" valign="top"><input name="" type="text" size="15"></td>
+    </tr>
+    
     <tr>
       <td align="left" valign="top">&nbsp;</td>
       <td align="left" valign="top">&nbsp;</td>
     </tr>
     <tr>
-      <td colspan="2" align="left" valign="top"><p><span><strong>Company and Faciility Information below.</strong></span></p></td>
+      <td collabel="2" align="left" valign="top">
+        <p><span><strong>Company and Facility Information below.</strong></span></p>
+      </td>
     </tr>
     <tr>
       <td align="left" valign="top">&nbsp;</td>
       <td align="left" valign="top">&nbsp;</td>
     </tr>
     <tr>
-      <td align="left" valign="top" style="padding:3px"><p><span>Facility Name:</span></p></td>
+      <td align="left" valign="top" style="padding:3px">
+        <p><label>Facility Name:</label></p>
+      </td>
       <td align="left" valign="top"><input name="" type="text" size="15"></td>
     </tr>
     <tr>
-      <td align="left" valign="top" style="padding:3px"><p><span>Facility Address:</span></p></td>
+      <td align="left" valign="top" style="padding:3px">
+        <p><label>Facility Address:</label></p>
+      </td>
       <td align="left" valign="top"><input name="" type="text" size="15"></td>
     </tr>
     <tr>
-      <td align="left" valign="top" style="padding:3px"><p><span>Facility Latitude:</span></p></td>
+      <td align="left" valign="top" style="padding:3px">
+        <p><label>Facility Latitude:</label></p>
+      </td>
       <td align="left" valign="top"><input name="" type="text" size="15"></td>
     </tr>
     <tr>
-      <td align="left" valign="top" style="padding:3px"><p><span>Facility Longitude:</span></p></td>
+      <td align="left" valign="top" style="padding:3px">
+        <p><label>Facility Longitude:</label></p>
+      </td>
       <td align="left" valign="top"><input name="" type="text" size="15"></td>
     </tr>
     <tr>
-      <td align="left" valign="top" style="padding:3px"><p><span>Facility - Contact Name:</span></p></td>
+      <td align="left" valign="top" style="padding:3px">
+        <p><label>Facility - Contact Name:</label></p></td>
       <td align="left" valign="top"><input name="" type="text" size="15"></td>
     </tr>
     <tr>
-      <td align="left" valign="top" style="padding:3px"><p><span>Facility - Phone (Mobile):</span></p></td>
+      <td align="left" valign="top" style="padding:3px"><p><label>Facility - Phone (Mobile):</label></p></td>
       <td align="left" valign="top"><input name="" type="text" size="15"></td>
     </tr>
     <tr>
-      <td align="left" valign="top" style="padding:3px"><p><span>Facility Email:</span></p></td>
+      <td align="left" valign="top" style="padding:3px"><p><label>Facility Email:</label></p></td>
       <td align="left" valign="top"><input name="" type="text" size="15"></td>
     </tr>
     <tr>
-      <td align="left" valign="top" style="padding:3px"><p><span>Facility ID Number:</span></p></td>
+      <td align="left" valign="top" style="padding:3px"><p><label>Facility ID Number:</label></p></td>
       <td align="left" valign="top"><input name="" type="text" size="15"></td>
     </tr>
     <tr>
-      <td align="left" valign="top" style="padding:3px"><p><span>Facility - Sector:</span></p></td>
+      <td align="left" valign="top" style="padding:3px"><p><label>Facility - Sector:</label></p></td>
       <td align="left" valign="top"><input name="" type="text" size="15"></td>
     </tr>
     <tr>
-      <td align="left" valign="top" style="padding:3px"><p><span>Facility - SIC Code:</span></p></td>
+      <td align="left" valign="top" style="padding:3px"><p><label>Facility - SIC Code:</label></p></td>
       <td align="left" valign="top"><input name="" type="text" size="15"></td>
     </tr>
     <tr>
-      <td align="left" valign="top" style="padding:3px"><p><span>Company Name:</span></p></td>
+      <td align="left" valign="top" style="padding:3px"><p><label>Company Name:</label></p></td>
       <td align="left" valign="top"><input name="" type="text" size="15"></td>
     </tr>
     <tr>
-      <td align="left" valign="top" style="padding:3px"><p><span>Company - Contanct Name:</span></p></td>
+      <td align="left" valign="top" style="padding:3px"><p><label>Company - Contanct Name:</label></p></td>
       <td align="left" valign="top"><input name="" type="text" size="15"></td>
     </tr>
     <tr>
-      <td align="left" valign="top" style="padding:3px"><p><span>Company - Phone (Mobile):</span></p></td>
+      <td align="left" valign="top" style="padding:3px"><p><label>Company - Phone (Mobile):</label></p></td>
       <td align="left" valign="top"><input name="" type="text" size="15"></td>
     </tr>
     <tr>
-      <td align="left" valign="top" style="padding:3px"><p><span>Company Email:</span></p></td>
+      <td align="left" valign="top" style="padding:3px"><p><label>Company Email:</label></p></td>
       <td align="left" valign="top"><input name="" type="text" size="15"></td>
     </tr>
     <tr>
-      <td align="left" valign="top" style="padding:3px"><p><span>Company Address:</span></p></td>
+      <td align="left" valign="top" style="padding:3px"><p><label>Company Address:</label></p></td>
       <td align="left" valign="top"><input name="" type="text" size="15"></td>
     </tr>
     <tr>
-      <td align="left" valign="top" style="padding:3px"><p><span>Company ID Number:</span></p></td>
+      <td align="left" valign="top" style="padding:3px"><p><label>Company ID Number:</label></p></td>
       <td align="left" valign="top"><input name="" type="text" size="15"></td>
     </tr>
     <tr>
