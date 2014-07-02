752b53aa221ffa268c80713c8aa579ddd8220e3b
WebDevJL <webdev.jl@gmail.com>
2014-07-02 07:35:06 +0200

    Add debug mode to test WebServices

--- Applications/PMTool/Templates/footer.php
+++ Applications/PMTool/Templates/footer.php
@@ -4,6 +4,7 @@
     <script type="application/javascript" src="<?php echo $this->app->relative_path; ?>Web/js/core/ko.js"></script>
     <script type="application/javascript" src="<?php echo $this->app->relative_path; ?>Web/js/core/bootstrap.min.js"></script>  
     <script type="application/javascript" src="<?php echo $this->app->relative_path; ?>Web/js/addons/toastr.js"></script>  
+    <script type="application/javascript" src="<?php echo $this->app->relative_path; ?>Web/js/app/debugger.js"></script>  
     <?php echo $this->app->globalResources["js_files_html"]; ?>
   </body>
 </html>
--- Web/pmtool.php
+++ Web/pmtool.php
@@ -7,7 +7,7 @@ ini_set("display_errors", 1);
 try {
   $app = new Applications\PMTool\PMToolApplication;
   //Only run the requested view if logged in
-  if (strstr($app->HttpRequest->requestURI(), "login") !== FALSE ||
+  if (isset($_GET["debug"]) || strstr($app->HttpRequest->requestURI(), "login") !== FALSE ||
           strstr($app->HttpRequest->requestURI(), "auth") !== FALSE ||
           $app->user->isAuthenticated()) {
     $app->run();
