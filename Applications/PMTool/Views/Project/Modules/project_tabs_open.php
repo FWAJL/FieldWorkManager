b23c1dd94c02a50654e6e8e8860a639d653ba33a
WebDevJL <webdev.jl@gmail.com>
2014-08-13 06:43:12 +0200

    Tab system > Rewrite the tab js code to be simplier

--- Applications/PMTool/Views/Project/Modules/project_tabs_open.php
+++ Applications/PMTool/Views/Project/Modules/project_tabs_open.php
@@ -1,8 +1,8 @@
 <?php if (!defined('__EXECUTION_ACCESS_RESTRICTION__')) exit('No direct script access allowed'); ?>
 
 <div id="tab-container" class="tab-container">
-  <ul class='etabs'>
-    <li class='tab'><a href="#project_info">Project Info</a></li>
-    <li class='tab'><a href="#facility_info">Facility Info</a></li>
-    <li class='tab'><a href="#client_info">Client Info</a></li>
+  <ul class="etabs">
+    <li class="tab active" data-form-id="project_info"><a>Project Info</a></li>
+    <li class="tab" data-form-id="facility_info"><a>Facility Info</a></li>
+    <li class="tab" data-form-id="client_info"><a>Client Info</a></li>
   </ul>
--- Web/js/app/controllers/tabs.js
+++ Web/js/app/controllers/tabs.js
@@ -1,3 +1,15 @@
     $(document).ready( function() {
-      $('#tab-container').easytabs();
+     //$('#tab-container').easytabs();
+     //init tabs
+     var _default = "project_info";
+     $(".data-form").hide();
+     $("#"+_default).show();
+     //manage click events
+     $(".tab").click(function () {
+      var data_form_id = $(this).attr("data-form-id");
+      $(".data-form").hide();
+      $("#"+data_form_id).fadeIn();
+      $(this).siblings().removeClass("active");
+      $(this).css("display","").addClass("active");
+     });
     });
