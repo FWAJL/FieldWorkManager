d541a4bc97e192d8ccd5257ecc50e0bcc3e461cb
WebDevJL <webdev.jl@gmail.com>
2014-08-20 07:21:32 +0200

    Manage projects > Implement new ListAll route

--- Applications/PMTool/Views/Project/index.php
+++ Applications/PMTool/Views/Project/index.php
@@ -5,15 +5,6 @@
     <h1 class="legend"><?php echo $resx["project_h1"]; ?></h1>
     <p><?php echo $resx["project_welcome_message"]; ?></p>
   </section>
-  
-  <section class="project_list <?php echo $display_project_list ?>">
-    <h3><?php echo $resx["project_list_all_header"]; ?></h3>
-    <h4><?php echo $resx["h3_projects_active"]; ?></h4>
-    <select multiple id="project-data-active" class="form-control"></select>
-    <h4><?php echo $resx["h3_projects_inactive"]; ?></h4>
-    <select multiple id="project-data-inactive" class="form-control"></select>
-  </section>
-
 </section>
 </div><!-- END ROW DIV -->
 </div><!-- END CONTENT CONTAINER -->
