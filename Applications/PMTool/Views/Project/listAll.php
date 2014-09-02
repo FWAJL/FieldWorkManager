f9ff39917c932f22bdfc1ecdef1f544e76011ee5
WebDevJL <webdev.jl@gmail.com>
2014-09-02 06:48:02 +0200

    CSS tweaks

--- Applications/PMTool/Views/Project/listAll.php
+++ Applications/PMTool/Views/Project/listAll.php
@@ -1,7 +1,7 @@
 <?php if (!defined('__EXECUTION_ACCESS_RESTRICTION__')) exit('No direct script access allowed'); ?>
-<section class="right-aside main col-lg-9 col-md-9 col-sm-9">
+<div class="right-aside main col-lg-9 col-md-9 col-sm-9">
 
-  <section class="project_list">
+  <div class="project_list">
     <h3><?php echo $resx["project_list_all_header"]; ?></h3>
     <div class="content-container container-fluid">
       <div class="row">
@@ -15,37 +15,33 @@
           foreach ($projects as $project) {
             if ($project->active) {
               echo "<li data-project-id=\"" . $project->project_id . "\" class=\"select_item ui-widget-content\">"
-                  . $project->project_name 
-                  . "</li>";
+              . $project->project_name
+              . "</li>";
             }
           }
           ?>              
         </ol>
-        <section class="col-lg-2 col-md-2">
-          <!-- Button to move from active to inactive -->
-          <input type="button" value="<?php echo $resx["btn_to_inactive_list"]; ?>" class="btn-primary from-active-list">
-
-          </input>
-          <!-- Button to move from active to inactive -->
-          <input type="button" value="<?php echo $resx["btn_to_active_list"]; ?>"  class="btn-primary from-inactive-list">
-
-          </input>
-        </section>
+        <div class="col-lg-2 col-md-2">
+          <div class="buttons">
+            <input type="button" value="<?php echo $resx["btn_to_inactive_list"]; ?>" class="btn btn-warning from-active-list" />
+            <input type="button" value="<?php echo $resx["btn_to_active_list"]; ?>"  class="btn btn-warning from-inactive-list" />            
+          </div>
+        </div>
         <ol id="inactive-list" class="list-panel col-lg-5 col-md-5">
           <?php
           foreach ($projects as $project) {
             if (!$project->active) {
               echo "<li data-project-id=\"" . $project->project_id . "\" class=\"select_item ui-widget-content\">"
-                  . $project->project_name 
-                  . "</li>";
+              . $project->project_name
+              . "</li>";
             }
           }
           ?>              
         </ol>
       </div>
     </div>
-  </section>
+  </div>
 
-</section>
+</div>
 </div><!-- END ROW DIV -->
 </div><!-- END CONTENT CONTAINER -->
