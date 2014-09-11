6690f48d4a6e8e121fc8cc703845ac21cb6f7f44
WebDevJL <webdev.jl@gmail.com>
2014-09-11 07:08:19 +0200

    Selfhost jquery-ui

---
+++ Applications/PMTool/Views/Modules/active_list.php
@@ -0,0 +1,32 @@
+<?php if (!defined('__EXECUTION_ACCESS_RESTRICTION__')) exit('No direct script access allowed'); ?>
+
+<div id="project_info"  class="data-form">
+<fieldset class="project_form">
+  <ol class="add-new-p">
+    <li style="display: none;">
+      <input name="project_id" type="text" />
+    </li>
+    <li>
+      <label><?php echo $resx["project_name"]; ?></label>
+      <input name="project_name" type="text" />
+    </li>
+    <li>
+      <label><?php echo $resx["project_num"]; ?></label>
+      <input name="project_num" type="text" />
+    </li>
+    <li>
+      <label><?php echo $resx["project_desc"]; ?></label>
+      <input name="project_desc" type="text" />
+    </li>
+    <li style="display: none">
+      <!--<label><?php // echo $resx["active"]; ?></label>-->
+      <input name="active" type="checkbox" />
+    </li>
+    <li style="display: none">
+      <label><?php // echo $resx["visible"]; ?></label>
+      <input name="visible" type="checkbox"/>
+    </li>
+  </ol>
+</fieldset>
+    
+</div>
