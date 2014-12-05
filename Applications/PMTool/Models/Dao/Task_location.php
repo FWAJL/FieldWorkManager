eaccbc558636e9a949ec2593a36449cfda371c49
WebDevJL <webdev.jl@gmail.com>
2014-12-05 07:11:29 +0100

    Issue #251
    Database update + Dao classes refreshed

---
+++ Applications/PMTool/Models/Dao/Task_location.php
@@ -0,0 +1,9 @@
+<?php
+namespace Applications\PMTool\Models\Dao;if ( ! defined('__EXECUTION_ACCESS_RESTRICTION__')) exit('No direct script access allowed');
+class Task_location extends \Library\Entity{  public     $task_id,    $location_id;
+  const     TASK_ID_ERR = 0,    LOCATION_ID_ERR = 1;
+  // SETTERS //  public function setTask_id($task_id) {      $this->task_id = $task_id;  }
+  public function setLocation_id($location_id) {      $this->location_id = $location_id;  }
+  // GETTERS //  public function task_id() {    return $this->task_id;  }
+  public function location_id() {    return $this->location_id;  }
+}
