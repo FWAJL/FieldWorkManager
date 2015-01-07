be26aea65c20ef8e36b2f3df0cb18aaca5d7910a
WebDevJL <webdev.jl@gmail.com>
2015-01-07 06:11:47 +0100

    Issue #377

---
+++ Applications/PMTool/Resources/Enums/AnalyteTabKeys.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ *
+ * @package		Basic MVC framework
+ * @author		FWM DEV Team
+ * @copyright	Copyright (c) 2014
+ * @license		
+ * @link		
+ * @since		
+ * @filesource
+ */
+// ------------------------------------------------------------------------
+
+/**
+ * AnalyteTabKeys Class
+ *
+ * @package		Applications/PMTool
+ * @subpackage	Resources/Enum
+ * @category	AnalyteTabKeys
+ * @author		FWM DEV Team
+ * @link		
+ */
+
+
+namespace Applications\PMTool\Resources\Enums;
+if ( ! defined('__EXECUTION_ACCESS_RESTRICTION__')) exit('No direct script access allowed');
+
+class AnalyteTabKeys {
+  const FieldTab = "atk_ft";
+  const LabTab = "atk_lt";
+}
