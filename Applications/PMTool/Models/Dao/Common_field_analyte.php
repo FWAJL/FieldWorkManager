ef9eae8d4710554bf42c350ed09cb70ad1a802e3
WebDevJL <webdev.jl@gmail.com>
2015-01-08 07:15:52 +0100

    Issue #387

---
+++ Applications/PMTool/Models/Dao/Common_field_analyte.php
@@ -0,0 +1,11 @@
+<?php
+/**** @package    Basic MVC framework* @author     Jeremie Litzler* @copyright  Copyright (c) 2014* @license* @link* @since* @filesource*/// ------------------------------------------------------------------------
+/**** Common_field_analyte Dao Class** @package     Application/PMTool* @subpackage  Models/Dao* @category    Common_field_analyte* @author      FWM DEV Team* @link*/
+namespace Applications\PMTool\Models\Dao;if ( ! defined('__EXECUTION_ACCESS_RESTRICTION__')) exit('No direct script access allowed');
+class Common_field_analyte extends \Library\Entity{  public     $common_field_analyte_id,    $common_field_analyte_name;
+  const     COMMON_FIELD_ANALYTE_ID_ERR = 0,    COMMON_FIELD_ANALYTE_NAME_ERR = 1;
+  // SETTERS //  public function setCommon_field_analyte_id($common_field_analyte_id) {      $this->common_field_analyte_id = $common_field_analyte_id;  }
+  public function setCommon_field_analyte_name($common_field_analyte_name) {      $this->common_field_analyte_name = $common_field_analyte_name;  }
+  // GETTERS //  public function common_field_analyte_id() {    return $this->common_field_analyte_id;  }
+  public function common_field_analyte_name() {    return $this->common_field_analyte_name;  }
+}
