e929452e8889b995dad9861e7e024908d3f53158
bsaiken <brianscottaiken@Brians-MacBook-Pro.local>
2015-01-16 13:27:58 -0700

    Issue #404
    Updates previous commit.  Now the forms are really blank

--- Applications/PMTool/Views/LiveTask/Modules/live_task_coc.php
+++ Applications/PMTool/Views/LiveTask/Modules/live_task_coc.php
@@ -1,71 +1,7 @@
 <?php if (!defined('__EXECUTION_ACCESS_RESTRICTION__')) exit('No direct script access allowed'); ?>
 
-<div id="coc_info"  class="data-form">
-<fieldset class="task_coc_form">
-  <ol class="add-new-item">
-    <li style="display: none;">
-      <input name="task_id" type="text" />
-    </li>
-        <li style="display: none;">
-      <input name="resource_id" type="text" />
-    </li>
-     <li><br /></li>
-    <li>
-      <label><?php echo $resx["po_number"]; ?></label>
-      <input name="po_number" type="text" />
-    </li>
-    <li>
-      <label><?php echo $resx["lab_instructions"]; ?></label>
-      <textarea name="lab_instructions" type="text"></textarea>
-    </li>
-    <li>
-      <label><?php echo $resx["lab_sample_type"]; ?></label>
-      <select class="coc_list" name="lab_sample_type">
-                                                        <option value="" selected="selected">Groundwater</option>
-                                                        <option value="">Surface Water</option>
-                                                        <option value="">Soil</option>
-                                                        <option value="">Air</option>
-                                                        <option value="">Other</option>
-                                                    </select>
-    </li>
-        <li>
-      <label><?php echo $resx["lab_sample_tat"]; ?></label>
-      <select class="coc_list" name="lab_sample_tat">
-                                                        <option value="" selected="selected">Standard</option>
-                                                        <option value="">1 week</option>
-                                                        <option value="">2 day</option>
-                                                        <option value="">see comments</option>
-                                                    </select>
-    </li>
-        <li>
-      <label><?php echo $resx["project_id_num"]; ?></label>
-      <input name="project_id_num" type="text" />
-    </li>
-    <li><br /></li>
-    <li><h3>Send Results to:</h3></li>
-    <li>
-      <label><?php echo $resx["results_to_name"]; ?></label>
-      <input name="results_to_name" type="text" />
-    </li>        
-    <li>
-      <label><?php echo $resx["results_to_company"]; ?></label>
-      <input name="results_to_company" type="text" />
-    </li>        
-    <li>
-      <label><?php echo $resx["results_to_address"]; ?></label>
-      <textarea name="results_to_address" type="text"></textarea>
-    </li>
-    <li>
-      <label><?php echo $resx["results_to_phone"]; ?></label>
-      <input name="results_to_phone" type="text" />
-    </li> 
+<div id="live_coc"  class="">
 
-    <li>
-      <label><?php echo $resx["results_to_email"]; ?></label>
-      <input name="results_to_email" type="text" />
-    </li>        
-  </ol>
-</fieldset>
     
 </div>
  
--- Applications/PMTool/Views/LiveTask/Modules/live_task_tabs_open.php
+++ Applications/PMTool/Views/LiveTask/Modules/live_task_tabs_open.php
@@ -2,53 +2,28 @@
 
 <div id="tab-container" class="tab-container">
   <ul class="etabs">
-    <li class="tab <?php echo $tab[Applications\PMTool\Resources\Enums\TaskTabKeys::InfoTab]; ?>" data-form-id="task_info">
-      <a href="<?php echo __BASEURL__ . Applications\PMTool\Helpers\TaskHelper::GetTaskInfoTabUrl($current_task); ?>">
-        Task Info
+    <li class="tab <?php echo $tab[Applications\PMTool\Resources\Enums\TaskTabKeys::LiveStatus]; ?>" data-form-id="task_status">
+      <a href="<?php echo __BASEURL__ . Applications\PMTool\Helpers\TaskHelper::GetTaskInfoTabUrl($current_task_status); ?>">
+        Task Status
       </a>
     </li>
-    <li class="tab <?php echo $tab[Applications\PMTool\Resources\Enums\TaskTabKeys::TechniciansTab]; ?>" data-form-id="task_technicians">
-      <a href="<?php echo __BASEURL__ . \Library\Enums\ResourceKeys\UrlKeys::TaskTechnicians; ?>">
-        Technicians
+    <li class="tab <?php echo $tab[Applications\PMTool\Resources\Enums\TaskTabKeys::LiveTaskMapTab]; ?>" data-form-id="live_task_map">
+      <a href="<?php echo __BASEURL__ . \Library\Enums\ResourceKeys\UrlKeys::LiveTaskMap; ?>">
+        Map
       </a>
     </li>
-    <li class="tab <?php echo $tab[Applications\PMTool\Resources\Enums\TaskTabKeys::LocationsTab]; ?>" data-form-id="task_locations">
-      <a href="<?php echo __BASEURL__ . \Library\Enums\ResourceKeys\UrlKeys::TaskLocations; ?>">
-        Locations
-      </a>
-    </li>
-    <li class="tab <?php echo $tab[Applications\PMTool\Resources\Enums\TaskTabKeys::InspFormsTab]; ?>" id="tab2" data-form-id="task_insp_info">
-      <a href="<?php echo __BASEURL__ . \Library\Enums\ResourceKeys\UrlKeys::TaskInspForms; ?>">
+    <li class="tab <?php echo $tab[Applications\PMTool\Resources\Enums\TaskTabKeys::LiveInspFormsTab]; ?>" id="tab2" data-form-id="live_task_insp_info">
+      <a href="<?php echo __BASEURL__ . \Library\Enums\ResourceKeys\UrlKeys::LiveTaskInspForms; ?>">
         Forms
       </a>
     </li>
-    <li class="tab <?php echo $tab[Applications\PMTool\Resources\Enums\TaskTabKeys::FieldAnalytesTab]; ?>" id="tab3" data-form-id="field_analytes">
-      <a href="<?php echo __BASEURL__ . \Library\Enums\ResourceKeys\UrlKeys::FieldAnalytes; ?>">
-        Field Analytes
-      </a>
-    </li>
-    <li class="tab <?php echo $tab[Applications\PMTool\Resources\Enums\TaskTabKeys::FieldSampleMatrixTab]; ?>" id="tab3a" data-form-id="field_matrix">
-      <a href="<?php echo __BASEURL__ . \Library\Enums\ResourceKeys\UrlKeys::FieldSampleMatrix; ?>">
-        Field Matrix
-      </a>
-    </li>
-    <li class="tab <?php echo $tab[Applications\PMTool\Resources\Enums\TaskTabKeys::CocTab]; ?>" id="tab4" data-form-id="task_coc_info">
-      <a href="<?php echo __BASEURL__ . \Library\Enums\ResourceKeys\UrlKeys::TaskCOC; ?>">
+    <li class="tab <?php echo $tab[Applications\PMTool\Resources\Enums\TaskTabKeys::LiveCocTab]; ?>" id="tab4" data-form-id="live_task_coc_info">
+      <a href="<?php echo __BASEURL__ . \Library\Enums\ResourceKeys\UrlKeys::LiveTaskCOC; ?>">
         Chain of Custody
       </a>
     </li>    
-    <li class="tab <?php echo $tab[Applications\PMTool\Resources\Enums\TaskTabKeys::LabAnalytesTab]; ?>" id="tab5" data-form-id="lab_analytes">
-      <a href="<?php echo __BASEURL__ . \Library\Enums\ResourceKeys\UrlKeys::LabAnalytes; ?>">
-        Lab Analytes
-      </a>
-    </li>
-    <li class="tab <?php echo $tab[Applications\PMTool\Resources\Enums\TaskTabKeys::LabSampleMatrixTab]; ?>" id="tab6" data-form-id="lab_matrix">
-      <a href="<?php echo __BASEURL__ . \Library\Enums\ResourceKeys\UrlKeys::LabSampleMatrix; ?>">
-        Lab Matrix
-      </a>
-    </li>
-    <li class="tab <?php echo $tab[Applications\PMTool\Resources\Enums\TaskTabKeys::ServicesTab]; ?>" id="tab7" data-form-id="task_services">
-      <a href="<?php echo __BASEURL__ . \Library\Enums\ResourceKeys\UrlKeys::TaskServices; ?>">
+    <li class="tab <?php echo $tab[Applications\PMTool\Resources\Enums\TaskTabKeys::LiveTaskServicesTab]; ?>" id="tab7" data-form-id="live_task_services">
+      <a href="<?php echo __BASEURL__ . \Library\Enums\ResourceKeys\UrlKeys::LiveTaskServices; ?>">
         Services
       </a>
     </li>
