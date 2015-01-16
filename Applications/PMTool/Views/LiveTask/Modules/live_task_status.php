147a529fc78bcb38041ef3cf47009c0a589ddd0b
bsaiken <brianscottaiken@Brians-MacBook-Pro.local>
2015-01-16 13:19:53 -0700

    Issue #404
    Created tabs and blank files for the upcoming Live Task UI.

---
+++ Applications/PMTool/Views/LiveTask/Modules/live_task_status.php
@@ -0,0 +1,141 @@
+<?php if (!defined('__EXECUTION_ACCESS_RESTRICTION__')) exit('No direct script access allowed'); ?>
+
+<div id="task_info"  class="data-form">
+  <fieldset class="task_form">
+    <ol class="add-new-item">
+      <li style="display: none;">
+        <input name="task_id" type="text" />
+      </li>
+      <li style="display: none;">
+        <input name="project_id" type="text" value="<?php echo $current_project->project_id() ?>" />
+      </li>
+      <br />
+      <li>
+        <label><?php echo $resx["task_name"]; ?></label>
+        <input name="task_name" type="text" />
+      </li>
+      <li>
+        <label><?php echo $resx["task_deadline"]; ?></label>
+        <input type="text"  name="task_deadline" id="datepicker"/>
+      </li>
+      <li>
+        <label><?php echo $resx["task_instructions"]; ?></label>
+        <textarea name="task_instructions" type="text"></textarea>
+      </li>
+      <li class="hide">
+        <label><?php echo $resx["task_active"]; ?></label>
+        <input name="task_active" type="checkbox"></textarea>
+      </li>
+    </ol>
+  </fieldset>
+</div>
+
+<!--      <label><?php //echo $resx["triggers"];   ?></label>
+      <table>
+        <tr>
+          <td class="checkbox"><input id="freq_list_box" type="checkbox" /><?php //echo $resx["task_trigger_cal"];   ?></td>
+          <td><select class="coc_list" id="freq_list">
+              <option value="" selected='selected'>Select</option>
+              <option value="Daily" >Daily</option>
+              <option value="Weekly">Weekly</option>
+              <option value="Two Weeks" >Two Weeks</option>
+              <option value="Monthly" >Monthly</option>
+              <option value="Two Months" >Two Months</option>
+              <option value="Quarterly" >Quarterly</option>
+              <option value="semi-Annual" >semi-Annual</option>
+              <option value="Annual" >Annual</option>
+            </select>  
+          </td>
+        <tr>
+          <td class="checkbox"><input name ="task_trigger_pm" id="manual_box" type="checkbox"><?php //echo $resx["task_trigger_pm"];   ?></td>
+          <td class="note"><div id="manual_note"><?php //echo $resx["trigger_pm_note"];   ?></div></td>
+        </tr>
+        <tr>
+          <td class="checkbox"><input id="external_box" type="checkbox"><?php //echo $resx["task_trigger_ext"];   ?></td>
+          <td id="external_note"><?php //echo $resx["trigger_ext_note"];   ?></td>
+        </tr>
+      </table>-->
+<!--<div id="task_info_data_collection" class="data-form-2">
+  <fieldset class="task_form_data_collection">
+    <ol class="add-new-item">
+      <li>
+        <label>Data Collection</label>
+        <table>
+          <tr>
+            <td class="checkbox"><input id="insp_note_box" type="checkbox" />Inspection</td>
+            <td id="insp_note">
+            </td>
+          </tr> 
+          <tr>
+            <td class="checkbox"><input id="field_data_box" type="checkbox" />Collect Field Data</td>
+            <td></td>
+          </tr> 
+
+          <tr>
+            <td class="checkbox"><input id='lab_sample_box' type="checkbox" />Collect Sample for Lab Analysis</td>
+            <td></td>
+          </tr>
+
+          <tr>
+            <td class="checkbox"><input id='service_providers_box' type="checkbox" />Check here if you need outside Service Providers </td>
+            <td></td>
+          </tr>
+        </table> 
+      </li>-->
+
+      <!--      <li>
+              <label><?php //echo $resx["resource_needs"];   ?></label>
+                                                        This table is an example - must be generated dynamically.
+              <table>                                  
+                <tr>
+                  <td><input id="well_box" type="checkbox">Drillers</td>
+                  <td style="width: 50px" id='tab7'>
+                    <select class="coc_list">
+                      <option value="" selected='selected'>Select</option>
+                      <option value="Daily" >Joe's Drilling</option>
+                      <option value="Weekly">Best Drilling</option>
+                      <option value="Two Weeks" >Deep Geotech</option>
+                    </select>  
+                  </td>
+                </tr>
+      
+                <tr>
+                  <td><input id="waste_box" type="checkbox" />Waste Disposal</td>
+                  <td style="width: 50px" id='tab8'>
+                    <select class="coc_list">
+                      <option value="" selected='selected'>Select</option>
+                      <option value="Daily" >No Waste Disposal Companies.  Update Resources (left).</option>
+                    </select>  
+                  </td>
+                </tr>
+                <tr>
+                  <td><input id="rental_box" type="checkbox" />Rent Equipment</td>
+                  <td style="width: 50px" id='tab9'>
+                    <select class="coc_list">
+                      <option value="" selected='selected'>Select</option>
+                      <option value="Daily" > Super Rental</option>
+                      <option value="Weekly">Enviro-rental</option>
+                      <option value="Two Weeks" >Rental Option 3</option>
+                    </select>  </td>
+                </tr> 
+                <tr>
+                  <td><input id="field_test_box" type="checkbox" />Field Test Kits</td>
+                  <td style="width: 50px" id='tab10'>
+                    <select class="coc_list">
+                      <option value="" selected='selected'>Select</option>
+                      <option value="Daily" >HACH</option>
+                      <option value="Weekly">LaMotte</option>
+                      <option value="Two Weeks" >Chemetrics</option>
+                    </select>  </td>
+                </tr>   
+                <tr>
+                  <td><input id="field_test_box" type="checkbox" />Other</td>
+                  <td style="width: 50px" id='tab10'>
+                  </td>
+                </tr>   
+              </table> 
+            </li>-->
+    </ol>
+  </fieldset>
+
+</div>
---
+++ Applications/PMTool/Views/LiveTask/Modules/live_task_tabs_open.php
@@ -0,0 +1,55 @@
+<?php if (!defined('__EXECUTION_ACCESS_RESTRICTION__')) exit('No direct script access allowed'); ?>
+
+<div id="tab-container" class="tab-container">
+  <ul class="etabs">
+    <li class="tab <?php echo $tab[Applications\PMTool\Resources\Enums\TaskTabKeys::InfoTab]; ?>" data-form-id="task_info">
+      <a href="<?php echo __BASEURL__ . Applications\PMTool\Helpers\TaskHelper::GetTaskInfoTabUrl($current_task); ?>">
+        Task Info
+      </a>
+    </li>
+    <li class="tab <?php echo $tab[Applications\PMTool\Resources\Enums\TaskTabKeys::TechniciansTab]; ?>" data-form-id="task_technicians">
+      <a href="<?php echo __BASEURL__ . \Library\Enums\ResourceKeys\UrlKeys::TaskTechnicians; ?>">
+        Technicians
+      </a>
+    </li>
+    <li class="tab <?php echo $tab[Applications\PMTool\Resources\Enums\TaskTabKeys::LocationsTab]; ?>" data-form-id="task_locations">
+      <a href="<?php echo __BASEURL__ . \Library\Enums\ResourceKeys\UrlKeys::TaskLocations; ?>">
+        Locations
+      </a>
+    </li>
+    <li class="tab <?php echo $tab[Applications\PMTool\Resources\Enums\TaskTabKeys::InspFormsTab]; ?>" id="tab2" data-form-id="task_insp_info">
+      <a href="<?php echo __BASEURL__ . \Library\Enums\ResourceKeys\UrlKeys::TaskInspForms; ?>">
+        Forms
+      </a>
+    </li>
+    <li class="tab <?php echo $tab[Applications\PMTool\Resources\Enums\TaskTabKeys::FieldAnalytesTab]; ?>" id="tab3" data-form-id="field_analytes">
+      <a href="<?php echo __BASEURL__ . \Library\Enums\ResourceKeys\UrlKeys::FieldAnalytes; ?>">
+        Field Analytes
+      </a>
+    </li>
+    <li class="tab <?php echo $tab[Applications\PMTool\Resources\Enums\TaskTabKeys::FieldSampleMatrixTab]; ?>" id="tab3a" data-form-id="field_matrix">
+      <a href="<?php echo __BASEURL__ . \Library\Enums\ResourceKeys\UrlKeys::FieldSampleMatrix; ?>">
+        Field Matrix
+      </a>
+    </li>
+    <li class="tab <?php echo $tab[Applications\PMTool\Resources\Enums\TaskTabKeys::CocTab]; ?>" id="tab4" data-form-id="task_coc_info">
+      <a href="<?php echo __BASEURL__ . \Library\Enums\ResourceKeys\UrlKeys::TaskCOC; ?>">
+        Chain of Custody
+      </a>
+    </li>    
+    <li class="tab <?php echo $tab[Applications\PMTool\Resources\Enums\TaskTabKeys::LabAnalytesTab]; ?>" id="tab5" data-form-id="lab_analytes">
+      <a href="<?php echo __BASEURL__ . \Library\Enums\ResourceKeys\UrlKeys::LabAnalytes; ?>">
+        Lab Analytes
+      </a>
+    </li>
+    <li class="tab <?php echo $tab[Applications\PMTool\Resources\Enums\TaskTabKeys::LabSampleMatrixTab]; ?>" id="tab6" data-form-id="lab_matrix">
+      <a href="<?php echo __BASEURL__ . \Library\Enums\ResourceKeys\UrlKeys::LabSampleMatrix; ?>">
+        Lab Matrix
+      </a>
+    </li>
+    <li class="tab <?php echo $tab[Applications\PMTool\Resources\Enums\TaskTabKeys::ServicesTab]; ?>" id="tab7" data-form-id="task_services">
+      <a href="<?php echo __BASEURL__ . \Library\Enums\ResourceKeys\UrlKeys::TaskServices; ?>">
+        Services
+      </a>
+    </li>
+  </ul>
