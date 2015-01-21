e0e7f0e6499e5c5e1366c9e187974b9533d80416
WebDevJL <webdev.jl@gmail.com>
2015-01-21 06:53:58 +0100

    Issue #420

--- Applications/PMTool/Views/Modules/upload_file.php
+++ Applications/PMTool/Views/Modules/upload_file.php
@@ -1,5 +1,5 @@
 <?php if (!defined('__EXECUTION_ACCESS_RESTRICTION__')) exit('No direct script access allowed'); ?>
-<form action="/FieldWorAssistantMVC/uploadFile" class="dropzone" id="document-upload">
+<form action="<?php echo $this->app->relative_path; ?>uploadFile" class="dropzone" id="document-upload">
   <div class="fallback">
     <input name="file" type="file" multiple />
   </div>
