567c33de852585ac8378e0ac6d23525c2069c08d
bsaiken <brianscottaiken@Brians-MacBook-Pro.local>
2015-01-25 11:59:10 -0700

    Issue #440
    Activate and deactive arrows instead of buttons.  See GIT for questions

--- Applications/PMTool/Views/Project/listAll.php
+++ Applications/PMTool/Views/Project/listAll.php
@@ -20,9 +20,13 @@
         </div>
         <div class="col-lg-2 col-md-2">
           <div class="buttons">
-            <input type="button" value="<?php echo $resx["btn_to_inactive_list"]; ?>" class="btn btn-warning from-active-list" />
-            <input type="button" value="<?php echo $resx["btn_to_active_list"]; ?>"  class="btn btn-warning from-inactive-list" />            
-            <input type="button" value="<?php echo $resx["btn_set_current_project"]; ?>"  class="btn btn-warning btn_set_current_project" />            
+<!--           <input type="button" value="<?php echo $resx["btn_to_inactive_list"]; ?>" class="btn btn-warning from-active-list" />-->
+            <button value="<?php echo $resx["btn_to_inactive_list"]; ?>" class="btn btn-warning from-active-list"> 
+             <span class="glyphicon glyphicon-arrow-right" title="De-activate"></span> </button>
+<!--           <input type="button" value="<?php echo $resx["btn_to_active_list"]; ?>"  class="btn btn-warning from-inactive-list" />-->
+            <button value="<?php echo $resx["btn_to_active_list"]; ?>" class="btn btn-warning from-inactive-list"> 
+             <span class="glyphicon glyphicon-arrow-left" title="Activate"></span> </button>
+           <input type="button" value="<?php echo $resx["btn_set_current_project"]; ?>"  class="btn btn-warning btn_set_current_project" />            
           </div>
         </div>
         <div  class="col-lg-5 col-md-5">
