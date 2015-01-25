c3ba4d0dfe7c2b149d477d60e2d4cce1afa8d6bf
bsaiken <brianscottaiken@Brians-MacBook-Pro.local>
2015-01-25 11:18:52 -0700

    Issue #439
    Task UI, Active Task UI, Complete Task UI Tab Keys and URL Keys

--- Applications/PMTool/Views/Modules/task_tabs_open.php
+++ Applications/PMTool/Views/Modules/task_tabs_open.php
@@ -4,27 +4,27 @@
   <ul class="etabs">
     <li class="tab <?php echo $tab[Applications\PMTool\Resources\Enums\TaskTabKeys::InfoTab]; ?>" data-form-id="task_info">
       <a href="<?php echo __BASEURL__ . Applications\PMTool\Helpers\TaskHelper::GetTaskInfoTabUrl($current_task); ?>">
-        Task Info
+        <?php echo $resx["task_tab_info"]; ?>
       </a>
     </li>
     <li class="tab <?php echo $tab[Applications\PMTool\Resources\Enums\TaskTabKeys::TechniciansTab]; ?>" data-form-id="task_technicians">
       <a href="<?php echo __BASEURL__ . \Library\Enums\ResourceKeys\UrlKeys::TaskTechnicians; ?>">
-        Technicians
+        <?php echo $resx["task_tab_technicians"]; ?>
       </a>
     </li>
     <li class="tab <?php echo $tab[Applications\PMTool\Resources\Enums\TaskTabKeys::LocationsTab]; ?>" data-form-id="task_locations">
       <a href="<?php echo __BASEURL__ . \Library\Enums\ResourceKeys\UrlKeys::TaskLocations; ?>">
-        Locations
+        <?php echo $resx["task_tab_locations"]; ?>
       </a>
     </li>
     <li class="tab <?php echo $tab[Applications\PMTool\Resources\Enums\TaskTabKeys::InspFormsTab]; ?>" id="tab2" data-form-id="task_insp_info">
       <a href="<?php echo __BASEURL__ . \Library\Enums\ResourceKeys\UrlKeys::TaskInspForms; ?>">
-        Forms
+        <?php echo $resx["task_tab_forms"]; ?>
       </a>
     </li>
     <li class="tab <?php echo $tab[Applications\PMTool\Resources\Enums\TaskTabKeys::FieldAnalytesTab]; ?>" id="tab3" data-form-id="field_analytes">
       <a href="<?php echo __BASEURL__ . \Library\Enums\ResourceKeys\UrlKeys::FieldAnalytes; ?>">
-        Field Analytes
+        <?php echo $resx["task_tab_field_analytes"]; ?>
       </a>
     </li>
 <!--    <li class="tab <?php //echo $tab[Applications\PMTool\Resources\Enums\TaskTabKeys::FieldSampleMatrixTab]; ?>" id="tab3a" data-form-id="field_matrix">
@@ -34,7 +34,7 @@
     </li>-->  
     <li class="tab <?php echo $tab[Applications\PMTool\Resources\Enums\TaskTabKeys::LabAnalytesTab]; ?>" id="tab5" data-form-id="lab_analytes">
       <a href="<?php echo __BASEURL__ . \Library\Enums\ResourceKeys\UrlKeys::LabAnalytes; ?>">
-        Lab Analytes
+        <?php echo $resx["task_tab_lab_analytes"]; ?>
       </a>
     </li>
 <!--    <li class="tab <?php //echo $tab[Applications\PMTool\Resources\Enums\TaskTabKeys::LabSampleMatrixTab]; ?>" id="tab6" data-form-id="lab_matrix">
@@ -44,12 +44,12 @@
     </li>-->
         <li class="tab <?php echo $tab[Applications\PMTool\Resources\Enums\TaskTabKeys::CocTab]; ?>" id="tab4" data-form-id="task_coc_info">
       <a href="<?php echo __BASEURL__ . \Library\Enums\ResourceKeys\UrlKeys::TaskCOC; ?>">
-        Chain of Custody
+        <?php echo $resx["task_tab_coc"]; ?>
       </a>
     </li>  
     <li class="tab <?php echo $tab[Applications\PMTool\Resources\Enums\TaskTabKeys::ServicesTab]; ?>" id="tab7" data-form-id="task_services">
       <a href="<?php echo __BASEURL__ . \Library\Enums\ResourceKeys\UrlKeys::TaskServices; ?>">
-        Services
+        <?php echo $resx["task_tab_services"]; ?>
       </a>
     </li>
   </ul>
