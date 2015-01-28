ae9cbe26ca60d4a40d869ffaf301f1bdefa9ad7f
WebDevJL <webdev.jl@gmail.com>
2015-01-28 06:33:45 +0100

    chmod

--- Library/Controllers/FileUploadController.php
+++ Library/Controllers/FileUploadController.php
@@ -21,13 +21,28 @@
  * @author		FWM DEV Team
  * @link		
  */
+
 namespace Library\Controllers;
-if ( ! defined('__EXECUTION_ACCESS_RESTRICTION__')) exit('No direct script access allowed');
+
+if (!defined('__EXECUTION_ACCESS_RESTRICTION__'))
+  exit('No direct script access allowed');
 
 class FileUploadController extends \Library\BaseController {
 
   public function executeUploadFile(\Library\HttpRequest $rq) {
-    \Library\Utility\DebugHelper::LogAsHtmlComment("Upload working");
+    $files = $this->files();
+    $dataPost = $this->dataPost();
+    if (!file_exists('path/to/directory')) {
+      mkdir('path/to/directory', 0777, true);
+    }
+    $dir = $this->app()->config()->get(\Library\Enums\AppSettingKeys::RootDocumentUpload);
+    //$dir = "";
+    if (move_uploaded_file(
+            $files['file']['tmp_name'], $dir . $files['file']['name'])) {
+      \Library\Utility\DebugHelper::LogAsHtmlComment($files['file']['name'] . " uploaded");
+    } else {
+      \Library\Utility\DebugHelper::LogAsHtmlComment($files['file']['name'] . " not uploaded");
+    }
   }
 
 }
