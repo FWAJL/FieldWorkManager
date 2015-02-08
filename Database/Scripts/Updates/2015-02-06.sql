950cb06c8a0ccbb5f7e47790c61cd83e2dcf0a0d
WebDevJL <webdev.jl@gmail.com>
2015-02-08 21:38:04 +0100

    Issue #488

--- Database/Scripts/Updates/2015-02-06.sql
+++ Database/Scripts/Updates/2015-02-06.sql
@@ -5,10 +5,10 @@
 USE baiken_fwm_1;
 
 ALTER TABLE `field_analyte`
-	ADD CONSTRAINT UNIQUE INDEX `un_cfa` (`field_analyte_name_unit` ASC);
+	ADD CONSTRAINT UNIQUE INDEX `un_fa` (`field_analyte_name_unit` ASC);
 ALTER TABLE `lab_analyte`
-	ADD CONSTRAINT UNIQUE INDEX `un_cfa` (`lab_analyte_name` ASC);
+	ADD CONSTRAINT UNIQUE INDEX `un_la` (`lab_analyte_name` ASC);
 ALTER TABLE `common_field_analyte`
 	ADD CONSTRAINT UNIQUE INDEX `un_cfa` (`common_field_analyte_name` ASC);
 ALTER TABLE `common_lab_analyte`
-	ADD CONSTRAINT UNIQUE INDEX `un_cfa` (`common_lab_analyte_name` ASC);
+	ADD CONSTRAINT UNIQUE INDEX `un_cla` (`common_lab_analyte_name` ASC);
