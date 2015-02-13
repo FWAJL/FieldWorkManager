cc90786c6c4af35a1ef3c14553d403699188b1a9
bsaiken <brianscottaiken@Brians-MacBook-Pro.local>
2015-02-13 16:35:44 -0700

    Removed deprecated files (including all index.php files).

--- Applications/PMTool/Views/Technician/index.php
+++
@@ -1,8 +0,0 @@
-<?php if (!defined('__EXECUTION_ACCESS_RESTRICTION__')) exit('No direct script access allowed'); ?>
-<section class="right-aside main col-lg-10 col-md-10 col-sm-10">
-  
-  <section class="welcome">
-    <h1 class="legend"><?php echo $resx["technician_h1"]; ?></h1>
-    <p><?php echo $resx["technician_welcome_message"]; ?></p>
-  </section>
-</section>
