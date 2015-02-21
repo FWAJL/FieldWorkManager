235657ef8483f142d244216f1e89852821b797c0
bsaiken <brianscottaiken@Brians-MacBook-Pro.local>
2015-02-21 09:43:07 -0700

    Issue #560
    Preliminary mods to change tooltips to headers (instead of each item).

--- Applications/PMTool/Views/Project/listAll.php
+++ Applications/PMTool/Views/Project/listAll.php
@@ -14,13 +14,14 @@
     </h3>
     <div class="content-container container-fluid">
       <div class="row">
-        <div  class="col-lg-5 col-md-5">
-          <h4><?php echo $resx["h3_projects_active"]; ?></h4>
+        <div class="col-lg-5 col-md-5">
+            <span class="h4"><?php echo $resx["h3_projects_active"]; ?></span>
+            <span class="glyphicon glyphicon-info-sign" id="active-project-header" title="Click on any Project to select : Right click to edit."></span>
           <?php require $active_list_module; ?>              
         </div>
         <div class="col-lg-1 col-md-1">
           <div class="buttons">
-<!--           <input type="button" value="<?php echo $resx["btn_to_inactive_list"]; ?>" class="btn btn-warning from-active-list" />-->
+<!--           <input type="button" value="<?php //echo $resx["btn_to_inactive_list"]; ?>" class="btn btn-warning from-active-list" />-->
             <button value="<?php echo $resx["btn_to_inactive_list"]; ?>" class="btn btn-warning from-active-list"> 
               <span class="glyphicon glyphicon-arrow-right" title="De-activate"></span> </button>
  <!--           <input type="button" value="<?php echo $resx["btn_to_active_list"]; ?>"  class="btn btn-warning from-inactive-list" />-->
@@ -30,7 +31,8 @@
           </div>
         </div>
         <div  class="col-lg-5 col-md-5">
-          <h4><?php echo $resx["h3_projects_inactive"]; ?></h4>
+          <span class="h4"><?php echo $resx["h3_projects_inactive"]; ?></span>
+            <span class="glyphicon glyphicon-question-sign" id="inactive-project-header" title="Inactive Projects do not auto-initiate any tasks"></span>
           <?php require $inactive_list_module; ?>
         </div>
         <?php
