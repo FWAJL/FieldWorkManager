0a119121b6643bf467ab3cb8b7028c7de7d9e433
WebDevJL <webdev.jl@gmail.com>
2015-03-03 21:11:58 +0100

    Pull request #601
    Merge with commit 273df3cb26190c00d4ba47fa767755baa2ab4090

--- Applications/PMTool/Views/Analyte/Modules/up_lab_analyte_lists.php
+++ Applications/PMTool/Views/Analyte/Modules/up_lab_analyte_lists.php
@@ -3,7 +3,8 @@
 <div class="content-container container-fluid">
   <div class="row">
     <div  class="col-lg-5 col-md-5">
-      <h4><?php echo $resx["h4_analyte_form"]; ?></h4>
+      <span class="h4"><?php echo $resx["h4_analyte_form"]; ?></span>
+      <span class="glyphicon glyphicon-question-sign" id="activequestion-labanalyte-uploadList-headerH4"></span>
       <?php require $form_modules[Applications\PMTool\Resources\Enums\ViewVariables\Analyte::analyte_buttons]; ?>
       <?php require $form_modules[Applications\PMTool\Resources\Enums\ViewVariables\Analyte::lab_analyte_form]; ?>
     </div>
@@ -11,7 +12,8 @@
       <?php require $form_modules[Applications\PMTool\Resources\Enums\ViewVariables\Analyte::common_analyte_buttons]; ?>
     </div>
     <div  class="col-lg-5 col-md-5">
-      <h4><?php echo $resx["h4_common_lab_analytes"]; ?></h4>
+      <span class="h4"><?php echo $resx["h4_common_lab_analytes"]; ?></span>
+      <span class="glyphicon glyphicon-question-sign" id="inactivequestion-labanalyte-uploadList-headerH4"></span>
       <?php require $form_modules[Applications\PMTool\Resources\Enums\ViewVariables\Analyte::common_lab_analyte_list]; ?>
     </div>
   </div>
--- Applications/PMTool/Views/Map/loadView.php
+++ Applications/PMTool/Views/Map/loadView.php
@@ -1,6 +1,7 @@
 <?php if (!defined('__EXECUTION_ACCESS_RESTRICTION__')) exit('No direct script access allowed'); ?>
 <div class="right-aside main col-lg-10 col-md-10 col-sm-10">
-  <h3><?php echo $resx["h3_title"]; ?><span class="glyphicon glyphicon-chevron-right"></span> <?php echo $resx["h3_sub_title_all_projects"]; ?></h3>
+  <span class="h3"><?php echo $resx["h3_title"]; ?><span class="glyphicon glyphicon-chevron-right"></span> <?php echo $resx["h3_sub_title_all_projects"]; ?></span>
+  <span class="glyphicon glyphicon-question-sign" id="question-map-h3"></span>
   <div class="content-container container-fluid">
       <div class="row">
           <div class="col-lg-10 col-md-10">
@@ -10,5 +11,6 @@
               <?php require $form_modules[\Applications\PMTool\Resources\Enums\ViewVariablesKeys::map_info_module]; ?>
           </div>
       </div>
+      <?php require $form_modules[\Applications\PMTool\Resources\Enums\PhpModuleKeys::tooltip_msg]; ?>
   </div>
 </div><!-- END RIGHT ASIDE MAIN -->
