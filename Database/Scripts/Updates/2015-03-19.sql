6b3afbfba58fcb6d756874d4124b87da0def216b
WebDevJL <webdev.jl@gmail.com>
2015-03-25 19:35:34 +0100

    Issue #421
    Update scripts updates

--- Database/Scripts/Updates/2015-03-19.sql
+++ Database/Scripts/Updates/2015-03-19.sql
@@ -5,7 +5,21 @@
 
 USE baiken_fwm_1;
 
--- Table structure for `user`
+-- Table structure for `user_role`
+CREATE TABLE `user_role` (
+    `user_role_id` smallint(2) NOT NULL,
+    `user_role_desc` varchar(100),
+    PRIMARY KEY (`user_role_id`)
+)  ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE = utf8_unicode_ci;
+
+INSERT INTO `user_role` (`user_role_id`,`user_role_desc`) VALUES
+(1,'Administrator'),
+(2,'Project Manager'),
+(3,'Field Technician'),
+(4,'Visitor'),
+(5,'None');
+
+-- Table structure for `web_user`
 CREATE TABLE `user` (
     `user_id` int(11) NOT NULL AUTO_INCREMENT,
     `user_login` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
@@ -15,22 +29,7 @@ CREATE TABLE `user` (
     `user_value` int(11) NOT NULL COMMENT 'ID value corresponding to the user_type',
     `user_role_id` smallint(2) NOT NULL COMMENT 'Look up the table user_role for details about the roles',
     `user_is_logged` tinyint(1) NOT NULL DEFAULT 0 COMMENT 'Flag to set to 1 when a user logs in successful. It will prevent multiple connection per single user',
-    CONSTRAINT `fk_ur_user` FOREIGN KEY (`user_role_id`)
+    CONSTRAINT `fk_user_role_user` FOREIGN KEY (`user_role_id`)
         REFERENCES `user_role` (`user_role_id`),
     PRIMARY KEY (`user_id`)
 )  ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE = utf8_unicode_ci AUTO_INCREMENT=1;
-
--- Table structure for `user_role`
-CREATE TABLE `user_role` (
-    `user_role_id` int(11) NOT NULL,
-    `user_role_desc` varchar(100)
-    PRIMARY KEY (`user_role_id`)
-)  ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE = utf8_unicode_ci;
-
-
-INSERT INTO `user_role` (`user_role_id`,`user_role_desc`) VALUES
-(1,'Administrator'),
-(2,'Project Manager'),
-(3,'Field Technician'),
-(4,'Visitor'),
-(5,'None');
