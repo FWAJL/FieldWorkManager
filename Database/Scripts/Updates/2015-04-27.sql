10704b2624be9beeda52a67eb75560caa1b428a0
FWAJL <j.litzler@fieldworkmanager.com>
2015-04-27 09:36:27 +0200

    Issue #916
    SQL updates

--- Database/Scripts/Updates/2015-04-27.sql
+++ Database/Scripts/Updates/2015-04-27.sql
@@ -6,5 +6,9 @@ USE baiken_fwm_1;
 ALTER TABLE `user`
 ADD COLUMN `user_email` VARCHAR(50) NOT NULL COMMENT 'User email that is unique and must be set';
 
+update `user`
+set `user_email` = `user_login`
+where `user_id` > 0;
+
 ALTER TABLE `user`
-ADD UNIQUE INDEX `un_user_email_val` (`user_email` ASC);
+ADD UNIQUE INDEX `un_user_email` (`user_email` ASC);
