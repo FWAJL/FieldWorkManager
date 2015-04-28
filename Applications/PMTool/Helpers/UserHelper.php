4584d701ab7d5274bc1e5a754c52e46d34f0e676
WebDevJL <webdev.jl@gmail.com>
2015-04-28 06:37:47 +0200

    Issue #916
    Final tweaks to add users to all instances.

--- Applications/PMTool/Helpers/UserHelper.php
+++ Applications/PMTool/Helpers/UserHelper.php
@@ -112,12 +112,16 @@ class UserHelper {
     );
   }
 
-  public static function PrepareUserObject($dataPost, $config, $setPass = FALSE) {
-    $user = CommonHelper::PrepareUserObject($dataPost, new \Applications\PMTool\Models\Dao\User());
+  public static function PrepareUserObject($dataPost,$config, $setPass = FALSE) {
+    $user = new \Applications\PMTool\Models\Dao\User();
     $protect = new \Library\BL\Core\Encryption();
-    if ($setPass == TRUE) {
+    $user->setUser_hint($dataPost['user_hint']);
+    $user->setUser_login($dataPost['user_login']);
+    $user->setUser_email($dataPost['pm_email']);
+    if($setPass==TRUE) {
       $user->setUser_password($protect->Encrypt($config->get("encryption_key"), $dataPost['user_password']));
     }
+
     return $user;
   }
 
@@ -159,7 +163,9 @@ class UserHelper {
       'user_type' => $user_type,
       'user_value' => $user_value
     );
-    $user = \Applications\PMTool\Helpers\UserHelper::PrepareUserObject($generatedDataPost, $caller->app()->config(), true);
+    $user = CommonHelper::PrepareUserObject($generatedDataPost, new \Applications\PMTool\Models\Dao\User());
+    $protect = new \Library\BL\Core\Encryption();
+    $user->setUser_password($protect->Encrypt($caller->app()->config()->get("encryption_key"), $user->user_password()));
     return $manager->add($user);
   }
   
