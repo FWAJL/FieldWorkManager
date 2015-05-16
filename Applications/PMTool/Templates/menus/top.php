2ba1ab5d8bb50a0688264bfc5da2b0ea7ca14ed4
bsaiken <brianscottaiken@dsn3.skype>
2015-05-16 08:52:43 -0700

    Issue #640
    Added three extra glyphicons, mentioned in my comments to the Mobile Site Integration Milestone.
    
    CSS was added to hide the new glyphicons (additional CSS can be added to the mobile.css file to show the glyphicons.
    
    I also removed unused view modules.

--- Applications/PMTool/Templates/menus/top.php
+++ Applications/PMTool/Templates/menus/top.php
@@ -7,6 +7,21 @@
     <span id="pm_name" class="top-right-option">
       <?php echo $resx_menu_left["p_user_name_label"]; ?><?php echo $pm['pm_name']; ?>
     </span>
+     <span id="ft_comm" class="top-right-option mobile-glyph">
+      <a class="glyphicon"  title="<?php echo $resx_menu_left["ft_comm"]; ?>">
+          <img src="../Web/images/glyphicons-246-chat.png">
+      </a>
+    </span>
+          <span id="ft_map" class="top-right-option mobile-glyph">
+      <a class="glyphicon" title="<?php echo $resx_menu_left["ft_map"]; ?>">
+          <img src="../Web/images/glyphicons-503-map.png">
+      </a>
+    </span>
+          <span id="ft_notes" class="top-right-option mobile-glyph">
+      <a class="glyphicon" title="<?php echo $resx_menu_left["ft_notes"]; ?>">
+          <img src="../Web/images/glyphicons-151-edit.png">
+      </a>
+    </span>     
     <span id="view_info" class="top-right-option">
       <a class="glyphicon glyphicon-user" title="<?php echo $resx_menu_left["pm_view_info"]; ?>"></a>
     </span>
