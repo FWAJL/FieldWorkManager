1aea42e9d37f7b4dc785149be4cc47f7c3d536e8
bsaiken <brianscottaiken@Brians-MacBook-Pro.local>
2015-07-21 11:26:51 -0500

    Issue #1098
    Sets up field data tab for active tasks.
    
    Has views, controller, and mockup of the field data table.

---
+++ Applications/PMTool/Views/ActiveTask/Modules/active_task_field_data.php
@@ -0,0 +1,51 @@
+<?php if (!defined('__EXECUTION_ACCESS_RESTRICTION__')) exit('No direct script access allowed'); 
+  //\Applications\PMTool\Helpers\CommonHelper::pr($form_modules);
+?>
+<div class="right-aside col-no-right-pad main col-lg-10 col-md-10 col-sm-10">
+<div id="field matrix container div for testing">	 
+<div class="matrix-row">
+	    	  	<div class="matrix-cell">&nbsp;</div>
+                        <div class="matrix-alalyte-header matrix-cell">Analytes</div>
+	    	  </div>
+	    	  <div class="matrix-row">
+	    	  	<div class="matrix-location-header matrix-cell">Location</div>
+			  						  		<div class="matrix-cell matrix-cell-data">
+					  			Counter value					  		</div>
+					  							  		<div class="matrix-cell matrix-cell-data">
+					  			Pump setti...<input type="hidden" class="ellipsis-tooltip" value="Pump setting (+/- in feet)" placement="top" >					  		</div>
+					  			    	  </div>
+	    	      		    	<div class="matrix-row">
+    		  	  	<div class="matrix-cell matrix-cell-data">
+    		  	  		MW-1    		  	  	</div>
+		    		  							  				<div class="matrix-cell matrix-cell-data">
+					  				<input type="checkbox" name="matrix_checkbox" class="matrix-checkbox" value="14_16"checked='checked' >
+					  				</div>
+					  	    						  				<div class="matrix-cell matrix-cell-data">
+					  				<input type="checkbox" name="matrix_checkbox" class="matrix-checkbox" value="14_17"checked='checked' >
+					  				</div>
+					  	    	    		  			</div>
+    		  		    		    	<div class="matrix-row">
+    		  	  	<div class="matrix-cell matrix-cell-data">
+    		  	  		MW-10    		  	  	</div>
+		    		  							  				<div class="matrix-cell matrix-cell-data">
+					  				123
+					  				</div>
+					  	    						  				<div class="matrix-cell matrix-cell-data">
+110					  				</div>
+					  	    	    		  			</div>
+    		  		    		    	<div class="matrix-row">
+    		  	  	<div class="matrix-cell matrix-cell-data">
+    		  	  		MW-11    		  	  	</div>
+		    		  							  				<div class="matrix-cell matrix-cell-data">
+Field Data					  				</div>
+					  	    						  				<div class="matrix-cell matrix-cell-data">
+More Data					  				</div>
+					  	    	    		  			</div>
+
+    </div>  
+
+		    				    
+
+
+    
+</div>
