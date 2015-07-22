12f544736d2185a7786ba9a5511db4db4de8b205
bsaiken <brianscottaiken@Brians-MacBook-Pro.local>
2015-07-22 00:13:15 -0500

    Issue #1072
    These changes will suffice for the time being.

--- Applications/PMTool/Views/Mobile/Modules/mobile_communications.php
+++ Applications/PMTool/Views/Mobile/Modules/mobile_communications.php
@@ -4,23 +4,20 @@
       <a id="refresh-chat" href="#"><span class="glyphicon glyphicon-refresh"></span></a> <span class="h4"><?php echo $resx['h4_taskcomm_leftcol'] ?></span>
       <div>
         <textarea  class="upload_list" name="task_comm_message" type="text"></textarea>
-        <div class="list-panel upload_list chatbox" name="task_comm_chatbox" id="task-comm-chatbox"></div>
-      </div>
-    </div>
-    <div class="col-lg-1 col-md-1">
-      <div class="buttons">
+            <div class="mobile_form_sections">
         <?php
         if ($comm_with_name !== '')
           require $form_modules["communications_button_module"];
         ?>
       </div>
-    </div>
-    <div  class="col-lg-5 col-md-5">
+        <div class="mobile_form_sections upload_list chatbox" name="task_comm_chatbox" id="task-comm-chatbox"></div>
+      </div>
+
       <span class="h4"><?php echo $resx['h4_taskcomm_rightcol'] ?></span>
       <div>
         <span class="h4"><?php echo $resx['h4_taskcomm_pm']; ?></span>
         <div class="scroll-bar">
-          <ol class="list-panel" id="pm-list">
+          <ol class="mobile_form_sections" id="pm-list">
             <li class="select_item ui-widget-content" data-activetask-id="<?php echo $current_pm->pm_id(); ?>"><?php echo $current_pm->pm_name();?></li>
           </ol>
 
@@ -28,14 +25,16 @@
       </div>
       <div>
         <span class="h4"><?php echo $resx["h4_taskcomm_services"]; ?></span>
+        <div class ="mobile_form_sections">
         <?php require $form_modules[Applications\PMTool\Resources\Enums\ViewVariablesKeys::categorized_list_left]; ?>
+        </div>
       </div>
       <div>&nbsp;
       </div>
       <div>
         <span class="h4"><?php echo $resx["h4_taskcomm_technicians"]; ?></span>
-        <?php require $form_modules["group_list_left"]; ?>
+        <div class ="mobile_form_sections"><?php require $form_modules["group_list_left"]; ?></div>
       </div>
-    </div>
   </div>
 </div>
+        </div>
