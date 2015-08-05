36fb4189fd8bfe74f220147aef0ae3bf1e23dbf9
WebDevJL <webdev.jl@gmail.com>
2015-08-05 06:36:21 +0200

    Issue #1075

--- Applications/PMTool/Views/Mobile/viewList.php
+++ Applications/PMTool/Views/Mobile/viewList.php
@@ -8,9 +8,10 @@
 
     <div class="row col-no-right-margin">
 
-      <div id="mobile-location-list" class="col-lg-2 col-md-2">
+      <div id="mobile-location-list" class="col-lg-4 col-md-4">
         <?php require $form_modules[\Applications\PMTool\Resources\Enums\ViewVariablesKeys::map_info_module]; ?>
       </div>
+      <div class="col-lg-8 col-md-8"></div>
     </div>
     <?php require $form_modules[\Applications\PMTool\Resources\Enums\PhpModuleKeys::tooltip_msg]; ?>
     <?php //require $form_modules[\Applications\PMTool\Resources\Enums\PhpModuleKeys::popup_maplegends_module]; ?>
