16c597c1b38ed624360a94b4b47f0cdfe8309ec9
WebDevJL <webdev.jl@gmail.com>
2015-08-23 20:42:08 +0200

    Issue #1197

--- Applications/PMTool/Views/TaskFieldAnalyte/Modules/analyte_list_buttons.php
+++ Applications/PMTool/Views/TaskFieldAnalyte/Modules/analyte_list_buttons.php
@@ -8,4 +8,9 @@
   <button title="<?php echo $resx["btn_remove_analyte_to_project"]; ?>"  class="btn btn-warning from-project-field-analyte-list">
     <span class="glyphicon glyphicon-arrow-left"></span>
   </button>
+  <a
+    class="task_add_location task_add_item btn btn-default"
+    href="<?php echo $this->app->relative_path; ?>analyte/uploadList?origin=task&originid=<?php echo $current_task->task_id(); ?>">
+      <?php echo $resx["task_field_data_button_add"]; ?>
+  </a>
 </div>
