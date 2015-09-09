2a567bcf509340c27fc5e123f46baf66498bfa79
Jérémie Litzler <webdev.jl@gmail.com>
2015-09-09 07:21:53 +0200

    Issue #1271
    mockup

---
+++ Mockups/issue-1271-mockup/top_menu.actions.js
@@ -0,0 +1,9 @@
+//top bar actions
+$(document).ready(function() {
+  $("#view_info").click(function() {
+    utils.redirect("user/showDetails");
+  });//Show pm info
+  $("#collapse-menu-button").on('click',function(){
+    $('#left-menu').toggle(400);
+});
+});
