535fdb40bad345edcc310fe39776d980199d75c1
Jérémie Litzler <webdev.jl@gmail.com>
2015-09-21 07:27:09 +0200

    Issue #1271
    reorganising menus.

---
+++ Applications/PMTool/Templates/menus/actionMenu.php
@@ -0,0 +1,12 @@
+<div class="action-item">
+  <span class="fa fa-list fa-2x"></span>
+</div>
+<div class="action-item">
+  <span class="fa fa-plus-circle fa-2x"></span>
+</div>
+<div class="action-item legacy-menu">
+  <span class="fa fa-bars fa-2x"></span>
+  <div class="hide">
+    <?php require __ROOT__ . 'Applications/PMTool/Templates/menus/legacyMenu.php'; ?>
+  </div>
+</div>
---
+++ Applications/PMTool/Templates/menus/branding.php
@@ -0,0 +1,3 @@
+<div id="branding">
+  <figure class="logo"><img src="<?php echo $this->app->relative_path . $this->app->logoImageUrl; ?>"></figure>
+</div>       
